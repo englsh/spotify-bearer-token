5e590b04914ed4804a436ed74da3230e0a822d14
englsh <75584941+englsh@users.noreply.github.com>
2023-01-05 22:20:46 +0000

    Update spot-test.py

--- spot-test.py
+++ spot-test.py
@@ -3,7 +3,7 @@ from spotifyApi import *
 
 searchTerm = "ocean man"
 
-Bearer = open("token.tkn","r").read()
+Bearer = open("token.tkn","r").read() # very simple way of storing bearer token, would reccomend encryption of some sort but i cba so
 
 if searchTrack(Bearer,searchTerm).get('tracks') == None:
     Bearer = return_access_token()
